6b081575e4d7e7494d3c49e78fb7ae544e111f51
Roni Palacio <roninfo@gmail.com>
2018-12-28 12:41:37 -0200

    add dependencia do Rabbon. Mapeia rabbon no projeto. Cria properties para indicar as urls para balanceamento

--- src/main/java/br/com/roninfo/proxy/CurrencyExchangeServiceProxy.java
+++ src/main/java/br/com/roninfo/proxy/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,15 @@
 package br.com.roninfo.proxy;
 
 import br.com.roninfo.model.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="forex-service", url="localhost:8000")
+// Com o uso do Ribbon para realizar o loadbalance, retiramos a url fixa.
+//@FeignClient(name="forex-service", url="localhost:8000")
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
