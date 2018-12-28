3bae2debf29ec7c5031d350246b10136cae39a09
Roni Palacio <roninfo@gmail.com>
2018-12-28 09:12:46 -0200

    first commit

---
+++ src/main/java/br/com/roninfo/microservicecurrencyconversion/MicroserviceCurrencyConversionApplication.java
@@ -0,0 +1,14 @@
+package br.com.roninfo.microservicecurrencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCurrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCurrencyConversionApplication.class, args);
+	}
+
+}
+
